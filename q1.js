96a12e0e92d2e523be254e4f83470229e8060c20
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-18 18:24:19 +0200

    answer 1

---
+++ q1.js
@@ -0,0 +1,8 @@
+let number1 = prompt("Input the First number", "0");
+let number2 = prompt("Input the second number", "0");
+
+    if(number1 > number2){
+        alert("number 1 is bigger");
+    }if(number1 < number2){
+        alert("number 2 is bigger");
+    }
