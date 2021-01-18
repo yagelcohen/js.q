bcdf3466606214377052f87c68f7f45085fd707a
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-18 18:43:41 +0200

    answer q2

---
+++ q2.js
@@ -0,0 +1,15 @@
+
+let x = -10;
+let y = 7;
+let a = 8;
+    if(x>0 && y>0 && a>0){
+         alert('the sing is +!')
+    }else if(x<0 && y<0 && a<0){
+         alert('the sing is -!')    
+    } else if(x>0 && y<0 && a<0){
+        alert('the sing is +!')
+    }else if(x>0 && y<0 && a>0){
+        alert('the sing is -!')
+    } else if(x<0 && y>0 && a<0){
+        alert('the sing is +!')  
+    } else {alert('the sing is -!')}       
