13312fb78789455b719084d13785db34f0c0548a
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-18 21:39:46 +0200

    answer q6

---
+++ q6.js
@@ -0,0 +1,19 @@
+let David =	80;
+let Vinoth = 77;
+let Divya =	88;
+let Ishitha	= 95;
+let Thomas = 68;
+const sum =(David+Vinoth+Divya+Ishitha+Thomas)/5;
+    if(sum<60){
+        console.log("F")
+    }else if(sum<70 && sum >=60){
+        console.log("D")
+    }else if(sum<80 && sum >=70){
+        console.log("C")
+    }else if(sum<90 && sum >=80){
+        console.log("B")
+    }else{
+        console.log("A")
+    }
+    console.log(sum)
+
